c33536f5b892b85212626a37956a6532dc5f418b
Raj-panel <rajsocialpanel@gmail.com>
2026-07-27 22:39:36 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,22 @@
+const SERVICE_MAP = {
+    instagram: {
+        followers: 1192,
+        likes: 784,
+        comments: 31,
+        shares: 49,
+        views: 10,
+        repost: 505,
+        photo_views: 1027
+    },
+
+    facebook: {
+        followers: 1283,
+        views: 1119,
+        shares: 460,
+        comments: 406
+    }
+};
+
 module.exports = async (req, res) => {
     try {
         const apiKey = process.env.WAVE_API_KEY;
@@ -11,10 +30,20 @@ module.exports = async (req, res) => {
 
         const action = req.query.action || "balance";
 
+        // Return our Service Mapping
+        if (action === "mapping") {
+            return res.status(200).json({
+                success: true,
+                services: SERVICE_MAP
+            });
+        }
+
         const params = new URLSearchParams();
+
         params.append("key", apiKey);
         params.append("action", action);
 
+        // Search Provider Services
         if (action === "search") {
             const query = req.query.query;
 
